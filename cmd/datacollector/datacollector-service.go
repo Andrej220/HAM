458da8bc4b8321d811d04b0a03aaeb4397026e85
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-05-05 22:26:21 +0200

    feat[datacollector] - add SendToDataservice to send data to dataservice

--- cmd/datacollector/datacollector-service.go
+++ cmd/datacollector/datacollector-service.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"encoding/json"
-	//"fmt"
 	"github.com/andrej220/HAM/internal/serverutil"
 	"github.com/andrej220/HAM/internal/workerpool"
+ gp "github.com/andrej220/HAM/internal/graphproc"
 	"github.com/google/uuid"
 	"log"
 	"net/http"
 	"sync"
 	"time"
+	"bytes"
 )
 
 const MAXTIMEOUT time.Duration = 1 * time.Minute
 const DATACOLLECTORPORT = "8081" 
+const DATASERVICEURL = "http://localhost:8082/dataservice"
 
 type datacollectorRequest struct {
 	HostID   int `json:"hostid"`
@@ -41,6 +44,34 @@ func newDatacollectorHandler() http.Handler {
 	return h
 }
 
+func SendToDataservice(gr *gp.Graph, httpClient *http.Client) error {
+	// TODO: log information about the request
+	graphBytes, err := json.Marshal(gr)
+	log.Println("Request to dataservice")
+	if err != nil {
+		return fmt.Errorf("failed to marshal graph: %v", err)
+	}
+
+	// Create HTTP POST request
+	req, err := http.NewRequest("POST", DATASERVICEURL, bytes.NewBuffer(graphBytes))
+	if err != nil {
+		return fmt.Errorf("failed to create request: %v", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	// Send request
+	resp, err := httpClient.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send request to dataservice: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("dataservice returned status %d", resp.StatusCode)
+	}
+	return nil
+}
+
 func (h *datacollectorHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	request, ok := r.Context().Value("request").(datacollectorRequest)
 	if !ok {
@@ -60,7 +91,14 @@ func (h *datacollectorHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request
 
 	jb := workerpool.Job[SSHJob]{
 		Payload: sshJob,
-		Fn:      sshrunner.RunJob,
+		Fn:     func(j SSHJob) error {
+					graph, err := RunJob(j)
+					if err != nil{
+						return err
+					}
+					SendToDataservice(graph,h.httpClient)
+					return nil
+				},
 		Ctx:     ctx,
 		CleanupFunc: func() {
 			if cancel, ok := h.cancelFuncs.Load(newUUID); ok {
@@ -74,6 +112,7 @@ func (h *datacollectorHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request
 	h.pool.Submit(jb)
 	response := datacollectorResponse{ExecutionUID: newUUID}
 
+	// TODO: think about the response
 	rw.Header().Set("Content-Type", "application/json")
 	rw.WriteHeader(http.StatusOK)
 	encoder := json.NewEncoder(rw)
