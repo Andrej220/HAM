44ed91ec0cc521a57dd42c684cf078203a554e61
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-06-01 12:37:38 +0200

    [datacollector] - update commands to delete pipline, add comments, automate creating ssh keys secret - git-ssh-key

--- cmd/datacollector/.tekton/apply.sh
+++ cmd/datacollector/.tekton/apply.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
-kubectl delete pipeline ham-deploy-pipeline
-kubectl delete pipelineRun ham-deploy-run
+echo "Deleting pipeline..."
+kubectl delete pipeline datacollector-deploy-pipeline
+kubectl delete pipelineRun datacollector-deploy-run
+# TODO: move it to cluster installation
+kubectl delete secret git-ssh-key
 
+echo "Creating/updating tasks and pipelines"
 kubectl apply -f task-build.yaml
 kubectl apply -f task-update-gitops.yaml
 kubectl apply -f pipeline.yaml
 kubectl apply -f pipeline-run.yaml
+
+echo "Creating/updating secrets with ssh keys"
+kubectl create secret generic git-ssh-key \
+  --from-file=ssh-privatekey=~.ssh/ham-gitops \
+  --from-file=ssh-publickey=.ssh/ham-gitpos.pub \
+  --from-literal=known_hosts="$(ssh-keyscan github.com)"
