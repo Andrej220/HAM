a0cb1c3e794bd843a964e0310aa765035dc59451
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-06-09 21:45:13 +0200

    [shared-models] - add execution uuid to request struct

--- pkg/shared-models/models.go
+++ pkg/shared-models/models.go
@@ -4,10 +4,10 @@ import(
 	"github.com/google/uuid"	
 )
 
-
 type Request struct {
 	HostID   int `json:"hostid"`
 	ScriptID int `json:"scriptid"`
+	ExecutionUID uuid.UUID `json:"exuid"`
 }
 
 type Response struct {
