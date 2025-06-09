16ad76b14bc98d357721796982682be03b6729a2
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-06-09 21:44:24 +0200

    [datacollectorProducer] - exted functions

--- apps/datacollectorProducer/producer.go
+++ apps/datacollectorProducer/producer.go
@@ -1,6 +1,6 @@
 // recives API requests and put in Kafka queue
 
-package main
+package producer
 
 import(
 	"net/http"
@@ -8,41 +8,102 @@ import(
 	"github.com/andrej220/HAM/pkg/serverutil"
 	"os"
 	"context"
-
+	"time"
+	dm "github.com/andrej220/HAM/pkg/shared-models"
+	"github.com/segmentio/kafka-go"
+	"encoding/json"
+	"errors"
+	"github.com/google/uuid"
 )
 
-const serviceName = "DATACOLLECTORPRODUCER"
-const servicePort = "8083"
-const HTTPpath = "/datacollectorProducer"
-const MAXTIMEOUT time.Duration = 1 * time.Minute
+const (
+	serviceName    = "DATACOLLECTORPRODUCER"
+	servicePort    = "8083"
+	HTTPpath       = "/datacollectorProducer"
+	MAXTIMEOUT     time.Duration = 2 * time.Minute
+	kafkaTopic     = "remote-requests" 
+	kafkaBrokers   = "localhost:9092"
+)
+
+type messageWriter interface {
+    WriteMessages(context.Context, ...kafka.Message) error
+    Close() error
+}
 
 type  Producer struct{
-
+	writer  messageWriter
+	lg 		lg.Logger
 }
 
-type httpRequest struct {
-
-}
 
 type Handler struct{
-	lg lg.Logger
+	producer 	*Producer
+	lg 			lg.Logger
+}
 
+func newKafkaProducer(lg lg.Logger) *Producer {
+	return &Producer{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(kafkaBrokers),
+			Topic:    kafkaTopic,
+			Balancer: &kafka.LeastBytes{},
+			Async:    false, 
+			AllowAutoTopicCreation: true,
+		},
+		lg: lg,
+	}
 }
 
 func newProducerHandler(lg lg.Logger) http.Handler {
-	handler := &Handler{lg:lg}
-	lg.Info("Created handler")
+	producer := newKafkaProducer(lg)
+	handler := &Handler{
+		producer: producer,
+		lg:       lg,
+	}
+	lg.Info("Created handler with Kafka producer")
 	return handler
 }
 
 func (h *Handler) ServeHTTP(rw http.ResponseWriter, r *http.Request){
-	request, ok := r.Context().Value("request").(httpRequest)
+	request, ok := r.Context().Value("request").(dm.Request)
 	if !ok {
 		http.Error(rw, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 	ctx, cancel := context.WithTimeout(lg.Attach(context.Background(), h.lg), MAXTIMEOUT)
-	
+
+	defer cancel()
+	// set new UUID to the request
+	request.ExecutionUID = uuid.New()
+
+	message, err := json.Marshal(request)
+	if err != nil {
+		h.lg.Error("Failed to marshal request", lg.Any("err", err))
+		http.Error(rw, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+	//h.lg.Info("prepare to send message")
+	err = h.producer.writer.WriteMessages(ctx,
+		kafka.Message{
+			Key:   request.ExecutionUID[:], 			// TODO:  add uuid 
+			Value: message,
+			Time:  time.Now(),
+		},
+	)
+	//h.lg.Info("Message is sent")
+
+	if err != nil {
+		if errors.Is(err, kafka.UnknownTopicOrPartition) {
+			h.lg.Error("Kafka topic does not exist", 
+				lg.String("topic", kafkaTopic),
+				lg.String("action", "Create the topic manually or enable auto-creation"))
+		}
+		http.Error(rw, "Failed to process request", http.StatusInternalServerError)
+		h.lg.Info("Failed to process request", lg.Any("ERROR",err))
+		return
+	}
+	rw.WriteHeader(http.StatusAccepted)
+	rw.Write([]byte("Request accepted and queued\n"))
 }
 
 
@@ -54,7 +115,7 @@ func main(){
 
 	mux := http.NewServeMux()
 	handler := newProducerHandler(logger)
-	mux.Handle(HTTPpath, serverutil.NewValidationHandler[httpRequest](handler))
+	mux.Handle(HTTPpath, serverutil.NewValidationHandler[dm.Request](handler))
 
 	config := serverutil.DefaultServerConfig()
 	config.Logger = logger
