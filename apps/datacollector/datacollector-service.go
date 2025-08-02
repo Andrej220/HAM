77f7ae8853f265e445d635a26a0790ab1c6b1993
Andrey Zargarov <andrey.zargarov@gmail.com>
2025-08-02 12:59:46 +0200

    [datacollector] - add graceful stop

--- apps/datacollector/datacollector-service.go
+++ apps/datacollector/datacollector-service.go
@@ -6,7 +6,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	//"os"
+	"os"
+	"os/signal"
+	"syscall"
 	"sync"
 	"time"
 	"github.com/andrej220/HAM/pkg/lg"
@@ -71,55 +73,6 @@ func SendToDataservice(gr *gp.Graph, httpClient *http.Client) error {
 	return nil
 }
 
-//func (h *datacollectorHandler) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-//	request, ok := r.Context().Value("request").(dm.Request)
-//	if !ok {
-//		http.Error(rw, "Internal server error", http.StatusInternalServerError)
-//		return
-//	}
-//	ctx, cancel := context.WithTimeout(lg.Attach(context.Background(), h.logger), MAXTIMEOUT)
-//	newUUID := uuid.New()
-//
-//	sshJob := SSHJob{
-//		HostID:   request.HostID,
-//		ScriptID: request.ScriptID,
-//		UUID:     newUUID,
-//		Ctx:      ctx,
-//	}
-//
-//	jb := workerpool.Job[SSHJob]{
-//		Payload: sshJob,
-//		Fn:     func(j SSHJob) error {
-//					graph, err := RunJob(j)
-//					if err != nil{
-//						return err
-//					}
-//					h.logger.Info("Request to dataservice")
-//					SendToDataservice(graph,h.httpClient)
-//					return nil
-//				},
-//		Ctx:     ctx,
-//		CleanupFunc: func() {
-//			if cancel, ok := h.cancelFuncs.Load(newUUID); ok {
-//				cancel.(context.CancelFunc)()
-//				h.cancelFuncs.Delete(newUUID)
-//			}
-//		},
-//	}
-
-//	h.cancelFuncs.Store(newUUID, cancel)
-//	h.pool.Submit(jb)
-//	response := dm.Response{ExecutionUID: newUUID}
-//
-//	// TODO: think about the response
-//	rw.Header().Set("Content-Type", "application/json")
-//	rw.WriteHeader(http.StatusOK)
-//	encoder := json.NewEncoder(rw)
-//	if err := encoder.Encode(response); err != nil {
-//		h.logger.Error("Failed to encode response: %v", lg.Any("err",err))
-//	}
-//}
-
 func Serve(data dm.Request, h *datacollectorHandler, ctx context.Context ) {
 
 	sshJob := SSHJob{
@@ -152,38 +105,79 @@ func Serve(data dm.Request, h *datacollectorHandler, ctx context.Context ) {
 }
 
 func main() {
-    cfg    := lg.NewConfigFromFlags(SERVICENAME)
-    logger := lg.New(cfg)
-	handler := newDatacollectorHandler(logger) 
+	cfg := lg.NewConfigFromFlags(SERVICENAME)
+	logger := lg.New(cfg)
+	handler := newDatacollectorHandler(logger)
 
-    consumerCfg := ku.Config{
-        Brokers: []string{
+	consumerCfg := ku.Config{
+		Brokers: []string{
 			"hev095wvtq2.sn.mynetname.net:31990",
 			"hev095wvtq2.sn.mynetname.net:31991",
 			"hev095wvtq2.sn.mynetname.net:31992",
 		},
-        Topic:   "orders",
-        GroupID: "order-service",
-    }
+		Topic:   "orders",
+		GroupID: "order-service",
+	}
 	fmt.Println("Kafka brokers:", consumerCfg.Brokers)
 	cons := ku.NewConsumer[dm.Request](consumerCfg)
-	defer cons.Close()	
+	defer cons.Close()
 
-	// TODO: add cancel function to shutdown the service gracefully
-	// TODO: catch ctrl+c signal 
-	ctx := context.Background()
-	for {
-		order, err := cons.Read(ctx)
-		if (err != nil){
-			logger.Error("error", lg.Any("err",err))
-			time.Sleep(time.Second)
-			continue
+	// Create a context that can be cancelled
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Set up channel for receiving signals
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Run the consumer in a goroutine so we can wait for signals
+	done := make(chan struct{})
+	go func() {
+		defer close(done)
+		for {
+			select {
+			case <-ctx.Done():
+				logger.Info("Shutting down consumer loop...")
+				return
+			default:
+				order, err := cons.Read(ctx)
+				if err != nil {
+					logger.Error("error", lg.Any("err", err))
+					time.Sleep(time.Second)
+					continue
+				}
+				logger.Debug("Received msg", lg.Any("order", order))
+				fmt.Println("Received msg:", order)
+				Serve(order, handler, ctx)
+			}
 		}
-		logger.Debug("Recieved msg",lg.Any("order",order))
-		fmt.Println("Recieved msg:", order)
-		Serve(order, handler, ctx)
+	}()
+
+	// Wait for signal or completion
+	select {
+	case sig := <-sigChan:
+		logger.Info("Received signal, shutting down...", lg.String("signal", sig.String()))
+		cancel() // Cancel the context to stop the consumer
+	case <-done:
+		logger.Info("Consumer finished normally")
 	}
-	//handler := newDatacollectorHandler(logger)
-	//exh := handler.(*datacollectorHandler)
-	//exh.pool.Stop()
+
+	// Wait for the consumer to finish
+	<-done
+
+	// Stop the handler pool
+	handler.pool.Stop()
+	logger.Info("Service shutdown completed")
 }
+
+
+
+//{"HostID":"1","ScriptID":"1","ExecutionUID":"1001"}
+//{"HostID":1,"ScriptID":1,"ExecutionUID":"1001"}
+//
+//./kafka-console-producer.sh   --bootstrap-server kafka-0.kafka-headless.kafka.svc.cluster.local:9092   --topic orders
+//>{"HostID":1,"ScriptID":1,"ExecutionUID":"1001"}
+//>{"HostID":1,"ScriptID":1,"ExecutionUID":"00000000-0000-0000-0000-000000000000"}
+//>{"HostID":1,"ScriptID":1,"ExecutionUID":"00000000-0000-0000-0000-000000000000"}
+//
+//kubectl exec -ti -n kafka kafka-0 -- bash
